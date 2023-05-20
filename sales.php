5de0215435c39ce4441645f9cb760f83a6451c6f
akbarnov <akbarnovian.an@gmail.com>
2023-05-20 21:23:43 +0700

    delete page not used

--- sales.php
+++
@@ -1,9 +0,0 @@
-        <?php require_once 'header.php'; ?>
-        
-        
-                <div class="container-fluid">
-                    <h3 class="text-dark mb-1">Sales</h3>
-                </div>
-            </div>
-        <?php require_once 'footer.php'; ?>
-            
--- supplier.php
+++
@@ -1,9 +0,0 @@
-        <?php require_once 'header.php'; ?>
-        
-        
-                <div class="container-fluid">
-                    <h3 class="text-dark mb-1">Supplier</h3>
-                </div>
-            </div>
-        <?php require_once 'footer.php'; ?>
-            
